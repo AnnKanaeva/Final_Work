ec8c478a3905bcb6f883d6620994edf5d0c4914d
Ann <anaykanaeva@yandex.ru>
2022-08-18 12:08:00 +0300

    Final_Work1

---
+++ Final_Work1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, 
+либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []*/
+
+Console.Write("Введите количество строк:\t");
+int elementscount = int.Parse(Console.ReadLine());
+string[] array1 = new string [elementscount];
+for (int i=0; i < array1.Length; i++)
+{
+    Console.Write ($"Введете строку {i+1}:\t");
+array1 [i] = (Console.ReadLine());}
+
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
